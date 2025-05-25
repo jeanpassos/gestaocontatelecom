ea180b6aca3d44c6b34b05de853d5cfbe1c42f14
Jean <jean@scsite.com.br>
2025-05-25 15:41:12 -0300

    revert: Remove migração para adicionar índice em segment.value devido a erros

--- telefonia-backend/core/db/migrations/005-add-index-to-segment-value.sql
+++
@@ -1,5 +0,0 @@
--- Definir o esquema como padrão para esta sessão
-SET search_path TO telefonia;
-
--- Adicionar um índice na coluna 'value' da tabela 'segment'
-CREATE INDEX IF NOT EXISTS idx_segment_value ON telefonia.segment(value);
